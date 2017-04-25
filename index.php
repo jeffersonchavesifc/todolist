a032227384c0baec4990521b46be5ad91659c1a0
jeffersonchavesifc <jefferson.chaves@ifc-araquari.edu.br>
2017-04-25 11:57:39 -0300

    Arquivos bases para o todo list

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require "logica.php"; ?>
+
+<html>
+
+    <head></head>
+
+    <body>
+        <h1>ola <?= "$tituloVisualizado $nome, $qualidadeVisualizada"; ?> </h1>
+    </body>
+
+</html>
---
+++ logica.php
@@ -0,0 +1,7 @@
+<?php
+$titulo    = ['Dom', 'Nobre', 'Paladino'];
+$qualidade = ['Abridor de galáxias', 'Salvador da pátria', 'O colosso de Araquari'];
+$nome      = "Jefferson";
+
+$tituloVisualizado    = $titulo[rand(0,2)];
+$qualidadeVisualizada = $qualidade[rand(0,2)];
